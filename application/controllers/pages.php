964a9643862c642af7367c6bcbbb01c128a8a046
kerryunicorns <kerry.mccalla@gmail.com>
2013-05-22 14:54:40 -0700

    add in code igniter project

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -2,15 +2,13 @@
 	class Pages extends CI_Controller{
 		
 		public function view($page='home'){
-			$test=constant('MAIN_FOLDER').'application/views/'.$page.'.php';
-			echo "<br/>\n page $page test   $test  exists ".file_exists($test);
-			if(!file_exists(constant('MAIN_FOLDER').'application/views/pages/'.$page.'.php')){
+			$thePage=constant('MAIN_FOLDER').'application/views/pages/'.$page.'.php';
+			if(!file_exists($thePage)){
 				//whoops, we don't have a page for that!
 				show_404();
 			}
 			
 			$data['title']=ucfirst($page); //Capitalize the first letter
-			echo "<br/>\n header $main_folder ".file_exists($main_folder.'/templates/header');
 			$this->load->view('templates/header',$data);
 			$this->load->view('pages/'.$page,$data);
 			$this->load->view('templates/footer',$data);
